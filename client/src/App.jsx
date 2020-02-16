8b54ea232e5ffe2964714d66784d754b61b4fdd6
BlakeCG <cristopherguerrero1994@gmail.com>
2020-02-16 00:08:09 -0500

    adding notes and base components, basic code in files, adding sudo code for later

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import './App.css';
+import './components/NavBar';
+import NavBar from './components/NavBar';
+import Navigation from './components/NavBar';
+import Button from '@material-ui/core/Button';
+import { BrowserRouter, Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/HomePage';
+import About from './components/ContactUs';
+import Recipes from './components/Recipes';
 
-class App extends React.Component {
-  state = { serverMessage: '' };
+// class App extends React.Component {
+//   state = { serverMessage: '' };
 
-  componentDidMount() {
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }));
-  }
+//   componentDidMount() {
+//     fetch('/api/demo')
+//       .then(response => response.json())
+//       .then(data => this.setState({ serverMessage: data.message }));
+//   }
 
-  render() {
-    return (
-      <div id="demo">
-        <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.serverMessage}</h1>
-      </div>
-    );
-  }
+//   render() {
+export default function App() {
+  return (
+    <>
+      <NavBar>
+        <Button>Home Logo</Button>
+        <Button>About Us</Button>
+        <Button>Search Form</Button>
+      </NavBar>
+    </>
+  );
 }
-
-export default App;
+// function PageRoutes() {
+//   <div></div>;
+// }
