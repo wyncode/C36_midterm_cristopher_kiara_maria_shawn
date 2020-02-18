62a9a6698dc29d523383a4b42472081e8594bfb8
BlakeCG <cristopherguerrero1994@gmail.com>
2020-02-17 20:04:58 -0500

    fixing API calls and displaying titles on main page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,44 +4,67 @@ import Input from '@material-ui/core/Input';
 import axios from 'axios';
 
 const SearchTacos = () => {
-  const [apiData, setApiData] = useState([])
-  const [query, setQuery] = useState('')    
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setQuery('')
-    const result = await axios.get('/api/recipes').then( res => {
-      console.log(res.data)
-    });
-    // make axios get request to backend 
-  }
+  const [apiData, setApiData] = useState([]);
+  const [query, setQuery] = useState('');
 
-  const HandleChange = (event) => {
-      setQuery(event.target.value)
-  }
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const result = await axios.get(`/api/recipes/${query}`);
+    setApiData(result.data);
+    // make axios get request to backend
+    setQuery('');
+  };
+
+  const HandleChange = event => {
+    setQuery(event.target.value);
+  };
 
   const useStyles = makeStyles(theme => ({
     root: {
       '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
+        margin: theme.spacing(1)
+      }
+    }
   }));
 
   const classes = useStyles();
 
   return (
-    <div style={{display:'flex', width:'100', justifyContent:'center', marginTop:'15rem'}}>
-    <form onSubmit={handleSubmit}  className={classes.root} noValidate autoComplete="off">
-       <Input onChange={HandleChange} value={query} placeholder="Look for Taco's Here!" inputProps={{ 'aria-label': 'description' }} />
-    </form>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          width: '100',
+          justifyContent: 'center',
+          marginTop: '15rem'
+        }}
+      >
+        <form
+          onSubmit={handleSubmit}
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+        >
+          <Input
+            onChange={HandleChange}
+            value={query}
+            placeholder="Look for Taco's Here!"
+            inputProps={{ 'aria-label': 'description' }}
+          />
+        </form>
+      </div>
+      {apiData.length ? (
+        apiData.map(recipe => {
+          return <p>{recipe.recipe.label}</p>;
+        })
+      ) : (
+        <h1>Search now!</h1>
+      )}
     </div>
   );
-}
+};
 
-export default SearchTacos
-
-
+export default SearchTacos;
 
 // adding a neat background image
 // search bar right in the middle
