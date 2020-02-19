5fea49706744e16db3f5f2f48626f392c00f5b39
BlakeCG <cristopherguerrero1994@gmail.com>
2020-02-19 18:44:14 -0500

    working on navbar fuctionality, removed search, removed favtacos

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,74 +3,45 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 // import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
-import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
-    width: 1300,
-    background: 'linear-gradient(45deg, #E4FF01 30%, #FF0101 90%)'
-  },
-  search: {}
+    background: 'linear-gradient(45deg, #E4FF01 30%, #FF0101 90%)',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexGrow: 1
+  }
 });
 
-export default function LabelBottomNavigation() {
+export default function SimpleBottomNavigation() {
   const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  // make navbar swap between pages
-  // make search function work.
   return (
-    <>
-      <BottomNavigation
-        value={value}
-        onChange={handleChange}
-        className={classes.root}
-      >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%'
-          }}
-        >
-          <Box>
-            <Link to="/">
-              <BottomNavigationAction
-                label="Home"
-                value="Home"
-                icon={<HomeIcon />}
-              />
-            </Link>
-            <Link to="/about">
-              <BottomNavigationAction
-                label="About Us"
-                value="About Us"
-                icon={<InfoIcon />}
-              />
-            </Link>
-            <Link to="/favorite-tacos">
-              <BottomNavigationAction
-                label="Favorite Taco Recipes"
-                value="favorites Taco Recipes"
-                icon={<FavoriteIcon />}
-              />
-            </Link>
-          </Box>
-          <Box>
-            <form className={classes.search} noValidate autoComplete="off">
-              <TextField id="standard-basic" label="Search for Tacos!" />
-            </form>
-          </Box>
-        </div>
-      </BottomNavigation>
-    </>
+    <BottomNavigation
+      showLabels
+      position="static"
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        component={Link}
+        to="/"
+        label="Home"
+        icon={<HomeIcon />}
+      />
+      <BottomNavigationAction
+        component={Link}
+        to="/about"
+        label="About Us"
+        icon={<InfoIcon />}
+      />
+    </BottomNavigation>
   );
 }
